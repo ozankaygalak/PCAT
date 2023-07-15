71e79b37d9d4789ab99ca09718ad078ebde2cafe
[Ozan Kaygalak] <[ozankaygalak@gmail.com]>
2023-07-15 13:11:40 +0300

    add requirement

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const app = express();
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  app.render('index.ejs')
+  //res.sendFile(path.resolve(__dirname, 'views/index.ejs'));
+  res.render('index')
+});
+app.get('/about',(req,res) =>{
+    res.render('about')
+});
+app.get('/contact',(req,res) =>{
+    res.render('contact')
 });
 app.set('view engine', 'ejs');
-const app = express();
-const port = 3000;
-app.listen(port, () => {console.log(`server ${port} de çalışıyor`)});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`server ${port} de çalışıyor`);
+});
