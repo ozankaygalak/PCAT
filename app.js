cc6ac514fd457acc6e94245689ea9f34000ddc90
[Ozan Kaygalak] <[ozankaygalak@gmail.com]>
2023-07-22 22:04:41 +0300

    runnable delete and update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 const app = express();
+const methodOverride = require('method-override');
 const fs = require('fs');
 const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
@@ -11,6 +12,9 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method',{
+  methods: ['POST','GET']
+}));
 
 mongoose.connect('mongodb://localhost:27017/pcat-db', {
   useNewUrlParser: true,
@@ -20,6 +24,16 @@ app.get('/photos/:id', async (req, res) => {
   try {
     const photo = await Photo.findById(req.params.id);
     res.render('photo', { photo }); // Pass the single 'photo' object, not an array
+  } catch (err) {
+    // Handle errors, e.g., photo not found in the database
+    res.status(404).send('Photo not found');
+  }
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  try {
+    const photo = await Photo.findOne({ _id: req.params.id });
+    res.render('edit', { photo }); // Pass the single 'photo' object, not an array
   } catch (err) {
     // Handle errors, e.g., photo not found in the database
     res.status(404).send('Photo not found');
@@ -40,19 +54,34 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
   res.render('contact');
 });
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.description = req.body.description;
+  photo.message = req.body.message;
+  photo.save();
+  res.redirect(`/photos/${req.params.id}`);
+});
+
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect('/');
+});
 
 app.post('/photos', async (req, res) => {
   const uploadDir = 'public/uploads';
 
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
-  };
+  }
   let uploadImage = req.files.image;
   let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
   uploadImage.mv(uploadPath, async () => {
     await Photo.create({
       ...req.body,
-      image: '/uploads/' + uploadImage.name
+      image: '/uploads/' + uploadImage.name,
     });
     res.redirect('/');
   });
